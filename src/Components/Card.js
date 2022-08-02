32174f4f9147e3b4f2a913f39dbcbcf04166f06f
Mujahidin1802 <mujahidin28394@gmail.com>
2022-08-02 20:15:28 +0700

    library-app

---
+++ src/Components/Card.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "/node_modules/react/cjs/react.development";
+import Modal from "./Modal";
+const Card = ({ book }) => {
+  const [show, setShow] = useState(false);
+  const [bookItem, setItem] = useState();
+  console.log(book);
+  return (
+    <>
+      {book.map((item) => {
+        
+        let thumbnail =
+          item.volumeInfo.imageLinks &&
+          item.volumeInfo.imageLinks.smallThumbnail;
+        let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+        if (thumbnail !== undefined && amount !== undefined) {
+          return (
+            <>
+              <div
+                className="card"
+                onClick={() => {
+                  setShow(true);
+                  setItem(item);
+                }}
+              >
+                <img src={thumbnail} alt="" />
+                <div className="bottom">
+                  <h3 className="title">{item.volumeInfo.title}</h3>
+                  <p className="amount">&#8377; {amount}</p>
+                </div>
+              </div>
+              <Modal
+                show={show}
+                item={bookItem}
+                onClose={() => setShow(false)}
+              />
+            </>
+          );
+        }
+      })}
+    </>
+  );
+};
+export default Card;
---
+++ src/Components/Main.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Card from "./Card";
+import axios from "axios";
+import "./style.css";
+const Main = () => {
+  const [search, setSearch] = useState("");
+  const [bookData, setData] = useState([]);
+  const searchBook = (evt) => {
+    if (evt.key === "Enter") {
+      axios
+        .get(
+          "https://www.googleapis.com/books/v1/volumes?q=" +
+            search +
+            "&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU" +
+            "&maxResults=40"
+        )
+        .then((res) => setData(res.data.items))
+        .catch((err) => console.log(err));
+    }
+  };
+
+  return (
+    <>
+      <div className="header">
+        <div className="row1">
+          <h1>
+            A room without books is like
+            <br /> a body without a soul.
+          </h1>
+        </div>
+        <div className="row2">
+          <h2>Find Your Book</h2>
+          <div className="search">
+            <input
+              id="keywords"
+              type="search"
+              placeholder="Enter Your Book Name"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              onKeyPress={searchBook}
+            />
+          </div>
+          <div className="image">
+            <img className="image-cartoon" src="./images/bg2.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="wrap-container">
+        <div className="container">{<Card book={bookData} />}</div>
+      </div>
+    </>
+  );
+};
+export default Main;
